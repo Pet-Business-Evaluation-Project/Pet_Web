993a932707e80fcdd749840e0424379cd7e7df82
Korcp <pm6567@naver.com>
2025-10-21 14:44:46 +0900

    feat : layout footer html 및 css 정의

---
+++ src/app/componets/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-700 py-6 text-center text-sm text-gray-300">
+      <div className="flex flex-col md:flex-row justify-center space-y-2 md:space-y-0 md:space-x-6">
+        <a href="#">사단법인 한국 반려동물 기업 협회 | 대표전화 : 031)906-0704 | 주소 : 경기도 고양시 일산동구 백성동 1309 우림보보카운티 1동 402호</a>
+      </div>
+      <p className="mt-2">Copyright ⓒ 2019 (사)한국 반려동물 기업협회 All right reserved. 사이트담당자 : 최우진</p>
+    </footer>
+  );
+}
