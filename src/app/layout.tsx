639e69c08c6c7c2b043bb5719d66611b67eaee44
Korcp <pm6567@naver.com>
2025-10-21 14:42:14 +0900

    feat : Header Footer main 레이아웃 잡음 , refactor : html lang en -> ko

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "./componets/Header/Header";
+import Footer from "./componets/Footer/Footer";
+import { ReactNode } from "react";
 
 
 
-export default function RootLayout({
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      
+    <html lang="ko">
+      <body className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1 bg-green-50">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
