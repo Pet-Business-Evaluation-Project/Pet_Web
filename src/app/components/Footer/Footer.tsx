335efab30c89cd3916c2372c451f8f0930134b44
Korcp <pm6567@naver.com>
2025-10-30 13:11:17 +0900

    refactor : Footer 정보 수정

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -2,9 +2,9 @@ export default function Footer() {
   return (
     <footer className="bg-gray-700 py-6 text-center text-sm text-gray-300">
       <div className="flex flex-col md:flex-row justify-center space-y-2 md:space-y-0 md:space-x-6">
-        <a href="#">사단법인 한국 반려동물 기업 협회 | 대표전화 : 031)906-0704 | 주소 : 경기도 고양시 일산동구 백성동 1309 우림보보카운티 1동 402호</a>
+        <a href="#">한국 기업 인증원 | 주소 : (10447)경기도 고양시 중앙로 1048 르메이에르 프라자 502호</a>
       </div>
-      <p className="mt-2">Copyright ⓒ 2019 (사)한국 반려동물 기업협회 All right reserved. 사이트담당자 : 최우진</p>
+      <p className="mt-2">Copyright ⓒ 2019 한국 기업 인증원 All right reserved. 사이트담당자 : 전천후</p>
     </footer>
   );
 }
