513e68143b0149c76473f604d9bd2f18d6629037
Korcp <pm6567@naver.com>
2025-10-31 14:57:57 +0900

    reafactor : 2019 -> 2025

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="flex flex-col md:flex-row justify-center space-y-2 md:space-y-0 md:space-x-6">
         <a href="#">한국 기업 인증원 | 주소 : (10447)경기도 고양시 중앙로 1048 르메이에르 프라자 502호</a>
       </div>
-      <p className="mt-2">Copyright ⓒ 2019 한국 기업 인증원 All right reserved. 사이트담당자 : 전천후</p>
+      <p className="mt-2">Copyright ⓒ 2025 한국 기업 인증원 All right reserved. 사이트담당자 : 전천후</p>
     </footer>
   );
 }
