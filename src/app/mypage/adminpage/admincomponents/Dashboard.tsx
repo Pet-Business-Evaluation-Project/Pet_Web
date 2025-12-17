e5819a40affd49c5ad0bb7063e72f050a6abddfd
박현수 <pm6567@naver.com>
2025-12-17 17:17:56 +0900

    refacotr : 영업비 수수료 수정 안된거 수정

--- src/app/mypage/adminpage/admincomponents/Dashboard.tsx
+++ src/app/mypage/adminpage/admincomponents/Dashboard.tsx
@@ -315,9 +315,9 @@ export default function Dashboard() {
       });
 
       const costTypes = [
-        { type: "charge", name: "영업비" },
+        { type: "charge", name: "수수료" },
         { type: "review", name: "심사비" },
-        { type: "invite", name: "수수료" },
+        { type: "invite", name: "영업비" },
         { type: "study", name: "강사비" },
       ];
 
@@ -689,9 +689,9 @@ export default function Dashboard() {
   const fetchPaymentStatistics = async () => {
     try {
       const costTypes = [
-        { type: "charge", name: "영업비" },
+        { type: "charge", name: "수수료" },
         { type: "review", name: "심사비" },
-        { type: "invite", name: "수수료" },
+        { type: "invite", name: "영업비" },
         { type: "study", name: "강사비" },
       ];
 
